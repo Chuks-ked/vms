d8866eb1f6e18f5086e299982ba3be5b28a882b8
Chuks-ked <ikedichukwubuikem@gmail.com>
2026-06-13 08:21:07 +0100

    modified exception handlers for a better response output

--- src/main/kotlin/com/vms/vms/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/vms/vms/exception/GlobalExceptionHandler.kt
@@ -1,3 +1,43 @@
 package com.vms.vms.exception
 
-class GlobalExceptionHandler(message: String) : RuntimeException(message)
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException::class)
+    fun handleResourceNotFound(
+        ex: ResourceNotFoundException,
+        request : HttpServletRequest
+    ): ResponseEntity<ErrorResponse> {
+
+        val errorPayload = ErrorResponse (
+            status = HttpStatus.NOT_FOUND.value(),
+            error = "Resource not found",
+            message = ex.message,
+            path = request.requestURI
+        )
+
+        return ResponseEntity(errorPayload, HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler(DuplicateResourceException::class)
+    fun handleDuplicateResource(
+        ex: DuplicateResourceException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorResponse> {
+
+        val errorPayload = ErrorResponse(
+            status = HttpStatus.CONFLICT.value(),
+            error = "Resource Already Exists",
+            message = ex.message,
+            path = request.requestURI
+        )
+
+        return ResponseEntity(errorPayload, HttpStatus.CONFLICT)
+    }
+}
