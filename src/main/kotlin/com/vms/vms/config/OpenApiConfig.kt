934cdc7aab2f31e22e9bee0473beaab097114129
Chuks-ked <ikedichukwubuikem@gmail.com>
2026-06-14 00:16:56 +0100

    Added Swagger configuration to accept token from the ui

---
+++ src/main/kotlin/com/vms/vms/config/OpenApiConfig.kt
@@ -0,0 +1,37 @@
+package com.vms.vms.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        val securitySchemeName = "BearerAuth"
+
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("VMS API Documentation")
+                    .version("1.0")
+                    .description("Visitor Management System Backend Endpoints")
+            )
+            .addSecurityItem(SecurityRequirement().addList(securitySchemeName))
+            .components(
+                Components().addSecuritySchemes(
+                    securitySchemeName,
+                    SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+            )
+    }
+}
